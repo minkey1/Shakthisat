9de3bb878b0a7a2b33308fbe50609c27433cf6bf
Minkey <vansh.bharadwaj2004@gmail.com>
2026-03-09 06:28:23 +0530

    fixed a lockfile and a minor fix in patrons page due to duplicate attributes

--- src/pages/PatronsOfHonor.tsx
+++ src/pages/PatronsOfHonor.tsx
@@ -290,8 +290,8 @@ const PatronsOfHonor = () => {
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="h-3.5 w-3.5">
                         <circle cx="11" cy="11" r="8" />
                         <line x1="21" y1="21" x2="16.65" y2="16.65" />
-                        <line x1="11" y1="8" x2="11" y1="14" />
-                        <line x1="8" y1="11" x2="14" y1="11" />
+                        <line x1="11" y1="8" x2="11" />
+                        <line x1="8" y1="11" x2="14" />
                       </svg>
                       View full size
                     </span>
